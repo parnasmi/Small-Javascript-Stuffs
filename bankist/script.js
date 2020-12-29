d103f519cfd912b6c2071982af3ff870ee994675
Maksadkulov Ilkhom <parnasmi@gmail.com>
2020-12-29 20:06:06 +0500

    feat: add nicely formatted&calculated dates

--- bankist/script.js
+++ bankist/script.js
@@ -17,9 +17,9 @@ const account1 = {
 		'2020-01-28T09:15:04.904Z',
 		'2020-04-01T10:17:24.185Z',
 		'2020-05-08T14:11:59.604Z',
-		'2020-05-27T17:01:17.194Z',
-		'2020-07-11T23:36:17.929Z',
-		'2020-07-12T10:51:36.790Z',
+		'2020-12-24T17:01:17.194Z',
+		'2020-12-28T23:36:17.929Z',
+		'2020-12-29T10:51:36.790Z',
 	],
 	currency: 'EUR',
 	locale: 'pt-PT', // de-DE
@@ -85,6 +85,24 @@ const currencies = new Map([
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+const calcDaysPassed = (date1, date2) => {
+	return Math.round(Math.abs((date1 - date2) / (1000 * 60 * 60 * 24)));
+};
+
+const formatDates = date => {
+	const daysPassed = calcDaysPassed(new Date(), date);
+	console.log('daysPassed', daysPassed);
+
+	if (daysPassed === 0) return 'today';
+	if (daysPassed === 1) return 'yesterday';
+	if (daysPassed <= 7) return `${daysPassed} days passed`;
+
+	const day = `${date.getDate()}`.padStart(2, 0);
+	const month = `${date.getMonth()}`.padStart(2, 0);
+	const year = date.getFullYear();
+	return `${day}/${month}/${year}`;
+};
+
 const displayContainer = (acc, sort) => {
 	const { movements, movementsDates } = acc;
 
@@ -94,12 +112,7 @@ const displayContainer = (acc, sort) => {
 		const type = mov > 1 ? 'deposit' : 'withdrawal';
 
 		const date = new Date(movementsDates[i]);
-
-		const day = `${date.getDate()}`.padStart(2, 0);
-		const month = `${date.getMonth()}`.padStart(2, 0);
-		const year = date.getFullYear();
-
-		const displayDate = `${day}/${month}/${year}`;
+		const displayDate = formatDates(date);
 
 		const html = `
       <div class="movements__row">
