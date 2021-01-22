12905188e7027a69a8f03c1c660201d562574804
Maksadkulov Ilkhom <parnasmi@gmail.com>
2021-01-22 08:01:30 +0500

    feat: add prettierrc

--- mapty/script.js
+++ mapty/script.js
@@ -10,3 +10,24 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+	pos => {
+		console.log('pos', pos);
+
+		var map = L.map('map').setView([51.505, -0.09], 13);
+
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			attribution:
+				'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		}).addTo(map);
+
+		L.marker([51.5, -0.09])
+			.addTo(map)
+			.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+			.openPopup();
+	},
+	() => {
+		console.log('error getting position');
+	}
+);
