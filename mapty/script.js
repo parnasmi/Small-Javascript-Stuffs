3ecaca4dd2a563f78c663d75546b0d14f0338503
Maksadkulov Ilkhom <parnasmi@gmail.com>
2021-02-02 08:14:37 +0500

    feat: render input form

--- mapty/script.js
+++ mapty/script.js
@@ -11,38 +11,55 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 navigator.geolocation.getCurrentPosition(
 	pos => {
 		const { latitude, longitude } = pos.coords;
 
 		const coords = [latitude, longitude];
 
-		var map = L.map('map').setView(coords, 13);
+		map = L.map('map').setView(coords, 13);
 
 		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 			attribution:
 				'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 		}).addTo(map);
 
-		map.on('click', function (mapEvent) {
-			const coords = Object.values(mapEvent.latlng);
+		map.on('click', function (mapE) {
+			mapEvent = mapE;
 
-			L.marker(coords)
-				.addTo(map)
-				.bindPopup(
-					L.popup({
-						maxWidth: 250,
-						minWidth: 100,
-						autoClose: false,
-						closeOnClick: false,
-						className: 'running-popup',
-					})
-				)
-				.setPopupContent('Workout')
-				.openPopup();
+			form.classList.remove('hidden');
+			inputDistance.focus();
 		});
 	},
 	() => {
 		console.log('error getting position');
 	}
 );
+
+form.addEventListener('submit', function (e) {
+	e.preventDefault();
+	const coords = Object.values(mapEvent.latlng);
+
+	inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+
+	L.marker(coords)
+		.addTo(map)
+		.bindPopup(
+			L.popup({
+				maxWidth: 250,
+				minWidth: 100,
+				autoClose: false,
+				closeOnClick: false,
+				className: 'running-popup',
+			})
+		)
+		.setPopupContent('Workout')
+		.openPopup();
+});
+
+inputType.addEventListener('change', function () {
+	inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+	inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+});
