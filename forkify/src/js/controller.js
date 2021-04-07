2289d85a053288780c42de74ddd8dab6f213d923
Maksadkulov Ilkhom <parnasmi@gmail.com>
2021-04-07 07:11:48 +0500

    refactor: add controller.js & update gitignore

---
+++ forkify/src/js/controller.js
@@ -0,0 +1,29 @@
+// import icons from '../img/icons.svg'; //Parcel 1
+import * as model from './model';
+import recipeView from './views/recipeView';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+// https://forkify-api.herokuapp.com/v2
+
+const showRecipeController = async function () {
+	try {
+		const id = window.location.hash.slice(1);
+		if(!id) return;
+		recipeView.renderSpinner();
+
+		//1) Loading recipe
+		await model.loadRecipe(id);
+
+		// 2) Render recipe
+		recipeView.render(model.state.recipe);
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+['load'].forEach(ev => window.addEventListener(ev, showRecipeController));
+
+// showRecipe();
+
+///////////////////////////////////////
