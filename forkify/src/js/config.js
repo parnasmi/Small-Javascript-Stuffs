76c8b9360037d23ac15e9bda4d38576e57f04f3d
Maksadkulov Ilkhom <parnasmi@gmail.com>
2021-04-12 06:59:12 +0500

    feat: uploading a new recipe 2

--- forkify/src/js/config.js
+++ forkify/src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
 export const TIMEOUT_SEC = 10;
 export const SEARCH_PER_PAGE = 10;
+export const ID_KEY = '10a5f032-aa17-4969-9a88-36c7fb0e3fa2';
+export const MODAL_CLOSE_TIMEOUT_SEC = 2.5;
--- forkify/src/js/controller.js
+++ forkify/src/js/controller.js
@@ -10,6 +10,7 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import RecipeView from './views/RecipeView';
 import { state } from './model';
+import {MODAL_CLOSE_TIMEOUT_SEC} from './config'
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -84,8 +85,27 @@ const addBookmarkController = () => {
 const recoverBookmarksController = () => {
 	BookmarksView.render(model.state.bookmarks);
 }
-const submitController = (data) => {
-	console.log('submit data', data)
+const submitController = async (data) => {
+	try {
+		//1) Render spinner
+		AddRecipeView.renderSpinner();
+		//2) Send data
+		await model.createRecipe(data)
+
+		//3) render recipe
+		RecipeView.render(state.recipe)
+
+		//4) Show Success message
+		AddRecipeView.renderSuccess('Recipe was successfully uploaded');
+
+		setTimeout(() => {
+			AddRecipeView.toggleModal();
+		}, MODAL_CLOSE_TIMEOUT_SEC * 1000)
+		console.log('state recipe', model.state.recipe)
+	} catch(ex) {
+		console.error(ex)
+		AddRecipeView.renderError(ex)
+	}
 }
 
 const init = function () {
